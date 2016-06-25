47924e014ebb4897c8f23ac408e2a5653336424b
Sultan Mira <sfm2686@g.rit.edu>
2016-06-24 23:22:58 -0400

    Made a class in every package

---
+++ src/Core/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Core;
+
+/**
+ * @authors Sultan Mira, Hunter Caskey
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/DataInterface/DBInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package DataInterface;
+
+/**
+ * @authors Sultan Mira, Hunter Caskey
+ *
+ */
+public class DBInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
