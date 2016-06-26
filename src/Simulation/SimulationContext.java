6b309ce8b73c66a432feac16ac61cc64c9fef73f
Sultan Mira <sfm2686@g.rit.edu>
2016-06-26 02:40:29 -0400

    simulation appears to work fine. Still needs better testing, cleaning.
    The system is now able to simulate off a simulation result and select
    the needed strategy.

--- src/Simulation/SimulationContext.java
+++ src/Simulation/SimulationContext.java
@@ -37,7 +37,7 @@ public class SimulationContext {
 			int timeSteps, String interval){
 		
 		//this.portfolio = portfolio;
-		this.growthRate = growthRate;
+		this.growthRate = growthRate / 100;
 		this.currentValue = value;
 		this.initValue = value;
 		this.steps = timeSteps;
@@ -70,9 +70,28 @@ public class SimulationContext {
 		return (double) this.simResults.get(this.count++);
 	}
 	
+	public void newSim(double growthRate, 
+			int timeSteps, String interval){
+		this.growthRate = growthRate / 100;
+		this.steps = timeSteps;
+		this.intervalNum = 1;
+		this.interval = interval;
+		this.currentValue = (double) this.simResults.get(this.simResults.size() - 1);
+		
+		if ( growthRate > 0 )
+			this.strategy = new BearMarketStrategy();
+		else if ( growthRate < 0 )
+			this.strategy = new BullMarketStrategy();
+		else
+			this.strategy = new NoGrowthMarketStrategy();
+		System.out.println("NEWSIM() IS CALLED ---------------------------");
+	}
+	
 	public ArrayList<Double> simulate(){
 		this.simResults = this.strategy.simulate(this.growthRate, this.currentValue,
 				this.steps, this.intervalNum);
+		System.out.println("SIMULATE() IS CALLED ...........................");
+		System.out.println(this.simResults);
 		return this.simResults;
 	}
 	
@@ -94,33 +113,47 @@ public class SimulationContext {
 	 */
 	public static void main(String[] args) {
 		//Testing .. 
+		int timeSteps;
+		double initValue;
+		double growthRate;
+		String interval;
+		String result;
+		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a value for the portfolio to simulate");
-		System.out.println("Options are:\n'year'\n'month'\n'day'");
-		System.out.print("Taking input: ");
-		String interval = input.nextLine();
+		System.out.print("Please enter time interval(year, month, day): ");
+		interval = input.nextLine();
 		System.out.print("Please enter time steps(integer): ");
-		int timeSteps = input.nextInt();
+		timeSteps = input.nextInt();
 		System.out.print("Please enter the portfolio's value(double): ");
-		double initValue = input.nextDouble();
+		initValue = input.nextDouble();
 		System.out.print("Please enter the growth rate(%): ");
-		double growthRate = input.nextDouble();
+		growthRate = input.nextDouble();
 		System.out.println("Assuming all of the information provided is correct, starting simulation...");
 		
-		SimulationContext test = new SimulationContext(growthRate / 100, initValue, timeSteps, interval);
+		SimulationContext test = new SimulationContext(growthRate, initValue, timeSteps, interval);
 		test.simulate();
 		
 		System.out.println("\nPlease press 'Enter' to view next value");
 		test.testingDisplay();
 		
-		/*
-		System.out.println("Testing input:");
-		System.out.println("Interval   => " + interval);
-		System.out.println("timeSteps  => " + timeSteps);
-		System.out.println("initValue  => " + initValue);
-		System.out.println("growthRate => " + growthRate);
-		*/
-		
+		while ( true ){
+			System.out.println("\nWould you like to simulate again?");
+			System.out.print("Please type 'Yes' or 'No': ");
+			result = input.nextLine();
+			System.out.println("YOU ENTERED " + result);
+			System.out.print("Please enter time interval(year, month, day): ");
+			interval = input.nextLine();
+			System.out.print("Please enter time steps(integer): ");
+			timeSteps = input.nextInt();
+			System.out.print("Please enter the growth rate(%): ");
+			growthRate = input.nextDouble();
+			test.newSim(growthRate, timeSteps, interval);
+			test.simulate();
+			System.out.println("\nPlease press 'Enter' to view next value");
+			test.testingDisplay();
+
+		}
+		//System.out.println("End of testing for simulationContext");
 	
 	}
 
