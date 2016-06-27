141797d14fe90891d11ec4adec38ff2b6adf34d2
huntercaskey <hunterrcaskey@gmail.com>
2016-06-27 19:56:22 -0400

    Completed the IndexUtil and Stock Util classes, and created unit tests for each.

--- src/CSV/EquityBin.java
+++ src/CSV/EquityBin.java
@@ -1,21 +1,25 @@
-import java.util.*;
 package CSV;
+import java.util.*;
 
 /**
  * @authors Sultan Mira, Hunter Caskey
- *
+ * 
+ * EquityBin is responsible for maintaining a listing of all available stocks and 
+ * indices within the system.
  */
 public class EquityBin {
 
+	
+	
+	public void addEquity(ArrayList<String> equity){
+		
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-	
-	public void addEquity(ArrayList<String> equity){
-		
 	}
 }
--- src/CSV/IndexUtil.java
+++ src/CSV/IndexUtil.java
@@ -1,9 +1,79 @@
 package CSV;
 
+import java.util.ArrayList;
+
+/*
+ * @author Hunter Caskey
+ */
 public class IndexUtil extends EquityUtil {
  
+	private ArrayList<StockUtil> stocks;
+	
+	public IndexUtil(String name){
+		super.setName(name);
+		this.stocks = new ArrayList<StockUtil>();
+	}
+	
+	public void addStock(StockUtil stock){
+		this.stocks.add(stock);
+	}
+	
+	public int getNumStocks(){
+		return stocks.size();
+	}
+	
+	public ArrayList<StockUtil> getStocks(){
+		return this.stocks;
+	}
+	
+	public boolean hasStock(StockUtil stock){
+		return stocks.contains(stock);
+	}
+
 	
 	public double getPrice(){
+		int numStocks = this.getNumStocks();
+		double price = 0.0;
 		
+		if (numStocks == 0){
+			return price;
+		}
+		for (StockUtil stock : this.stocks){
+			price += stock.getPrice();
+		}
+		return (price/numStocks);
+	}
+	
+	
+	public static void main(String[] args){
+		IndexUtil testIndex = new IndexUtil("Tech Companies");
+		StockUtil stock1 = new StockUtil("Apple", "APPL", "300");
+		StockUtil stock2 = new StockUtil("Google", "GOOG", "200");
+		StockUtil stock3 = new StockUtil("Microsoft", "MSFT", "1000");
+
+		testIndex.addStock(stock1);
+		testIndex.addStock(stock2);
+		testIndex.addStock(stock3);
+		
+		int testNum = 5;
+		int failCount = 0;
+		
+		if (testIndex.getNumStocks() != 3){
+			++failCount;
+		}
+		if (! testIndex.hasStock(stock1)){
+			++failCount;
+		}
+		if (! testIndex.hasStock(stock2)){
+			++failCount;
+		}
+		if (! testIndex.hasStock(stock3)){
+			++failCount;
+		}
+		if (testIndex.getPrice() != 500.0){
+			++failCount;
+		}
+		
+		System.out.println("Conducting unit tests for IndexUtil:\n" + (testNum - failCount) + " out of " + testNum + " tests passed.");
 	}
 }
