5bc8b184b6b4f05ff7440a380331c3237255557e
Sultan Mira <sfm2686@g.rit.edu>
2016-06-27 20:18:40 -0400

    Parsed the file, code is really messed up now cuz of all the testing

--- src/CSV/CSVParser.java
+++ src/CSV/CSVParser.java
@@ -1,5 +1,35 @@
 package CSV;
+import java.io.*;
+import java.net.URL;
 
+/**
+ * @authors Sultan Mira, Hunter Caskey
+ *
+ */
 public class CSVParser {
 
+	private String name = "./equities.csv";
+	
+	public CSVParser(){
+		String [] fields;
+		try(FileInputStream fi = new FileInputStream(this.name)) {
+			InputStreamReader in = new InputStreamReader(fi);
+			BufferedReader br = 
+					new BufferedReader(in);
+		    for(String row; (row = br.readLine()) != null; ) {
+		        fields = row.split(",");
+		        for ( int i = 0; i < fields.length; i ++ )
+		        	System.out.println("Field " + (i + 1) + ": " + fields[i]);
+		    }
+		} catch (IOException e) {
+			System.out.println("File not found!!");
+			e.printStackTrace();
+			return ;
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		CSVParser test = new CSVParser();
+	}
 }
