95ead759f59a08e0081ba016d4e50557ba2a31c8
huntercaskey <hunterrcaskey@gmail.com>
2016-06-27 20:23:41 -0400

    Merging

--- src/CSV/EquityBin.java
+++ src/CSV/EquityBin.java
@@ -9,8 +9,30 @@ import java.util.*;
  */
 public class EquityBin {
 
+	private Set<StockUtil> stockBin;
+	private Set<IndexUtil> indexBin;
+	private static EquityBin instance = null;
 	
+	public EquityBin getEquityBin(){
+		if (this.instance == null) {
+			instance = new EquityBin();
+		}
+		return this.instance;
+	}
 	
+	private EquityBin(){
+		this.stockBin = new HashSet<StockUtil>();
+		this.indexBin = new HashSet<IndexUtil>();
+	}
+	
+	/**
+	 * 
+	 * @param equity A collection of the fields of a given stock. 
+	 * 		The first index is the stock's ticker symbol.
+	 * 		The second index is the stock's name.
+	 * 		The third index is the stock's price. 
+	 * 		The remaining values of the collection specify the index/sector that the stock belongs to, if any.
+	 */
 	public void addEquity(ArrayList<String> equity){
 		
 	}
