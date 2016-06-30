f4ad37e2ea29ef84ba63dbd0bb7c55b0ca14a214
Sultan Mira <sfm2686@g.rit.edu>
2016-06-30 02:15:31 -0400

    Finished the last state, about to do testing .... :((((((((((((((

--- src/Core/AskNumPriceDest.java
+++ src/Core/AskNumPriceDest.java
@@ -3,6 +3,9 @@
  */
 package Core;
 
+import java.util.ArrayList;
+
+import CSV.EquityUtil;
 import Finance.*;
 
 /**
@@ -11,22 +14,95 @@ import Finance.*;
  */
 public class AskNumPriceDest extends State {
 	
-	/**
-	 * @param context
-	 */
-	public AskNumPriceDest(Context context) {
-		super(context);
-	}
 
 	private int id = 21;
 	private int i;
 	private Equity eq;
 	private CashAcct acct;
 	private Portfolio port;
+	private ArrayList<CashAcct> cashAccounts;
+	private ArrayList<Portfolio> portfolioMapping;
+	
+	
+	/**
+	 * @param context
+	 */
+	public AskNumPriceDest(Context context) {
+		super(context);
+		this.cashAccounts = new ArrayList<>();
+		this.portfolioMapping = new ArrayList<>();
+	}
 
 	//USed differently in this state 
 	@Override
 	void displayOptions() {
+		System.out.println("Cash Accounts:");
+		int i = 0;
+		System.out.println("If the destination of the fund is outside the system, enter 0");
+		for(Portfolio port : getContext().getUserPorts()){
+			System.out.println("\tCash Accounts from Portfolio: " + port.getName());
+			for(CashAcct acct : port.getCashAccounts()){
+				System.out.println("\t\t" + ++i + ". " + acct.toString());
+				this.cashAccounts.add(acct);
+				this.portfolioMapping.add(port);
+			}		
+		}
+		System.out.print("Please select an account to retrieve the required funds from (integer): ");
+	}
+
+	/* (non-Javadoc)
+	 * @see Core.State#execute()
+	 */
+	@Override
+	void execute() {
+		System.out.println("\n------Equity Selection-----\n");
+		this.selectEq();
+		
+		System.out.print("Please specify the number of shares you would like to sell (integer): ");
+		int shares = getSc().nextInt();
+		while ( shares > this.eq.getNumShares() ){
+			System.out.println("Invalid input, please try again");
+			System.out.print("Please specify the number of shares you would like to sell (integer): ");
+			shares = getSc().nextInt();
+		}
+		boolean failure;
+		int in = -1;
+		CashAcct dest = null;
+		do{
+			displayOptions();
+			failure = false;
+			try{
+				in = getSc().nextInt();
+				if(isValid(0, this.cashAccounts.size(), in)){
+					if(in != 0){
+						dest = this.cashAccounts.get(in - 1);
+					}
+				}
+				else{
+					failure = true;
+					System.out.println("Invalid input, please try again.");
+				}
+			}
+			catch(Exception e){
+				System.out.println("Invalid input, please try again.");
+				failure = true;
+			}
+		}while(failure);
+		
+		System.out.print("Please specify the sale price (double): ");
+		double price = getSc().nextDouble();
+		
+		if ( in == 0 ){
+			getContext().getTransClient().transferEquity(getContext().getPort(), this.eq, shares, price);
+		}
+		else{
+			getContext().getTransClient().transferEquity(getContext().getPort(),
+					this.eq, shares, price, this.portfolioMapping.get(in - 1), dest);
+		}
+		setNext(0);
+	}
+	
+	private void selectEq(){
 		this.i = 1;
 		for ( Equity eq : getContext().getPort().getEquities() )
 			System.out.println(this.i++ + ". " + eq.toString());
@@ -43,33 +119,6 @@ public class AskNumPriceDest extends State {
 		}
 		this.eq = getContext().getPort().getEquities().get(in - 1);
 	}
-
-	/* (non-Javadoc)
-	 * @see Core.State#execute()
-	 */
-	@Override
-	void execute() {
-		System.out.println("\n------Equity Selection-----\n");
-		this.displayOptions();
-		
-		System.out.print("Please specify the number of shares you would like to sell (integer): ");
-		int in = getSc().nextInt();
-		while ( in > this.eq.getNumShares() ){
-			System.out.println("Invalid input, please try again");
-			System.out.print("Please specify the number of shares you would like to sell (integer): ");
-			in = getSc().nextInt();
-		}
-		
-		System.out.print("Please specify the sale price (integer): ");
-		in = getSc().nextInt();
-		
-		System.out.println("\n\n");
-		System.out.println("Please select a destination Cash Account");
-		this.selectCashAcct();
-		
-		
-	}
-	
 	private void selectCashAcct(){
 		
 		//source portfolio selection
@@ -94,7 +143,7 @@ public class AskNumPriceDest extends State {
 		for ( CashAcct acct : this.port.getCashAccounts() )
 			System.out.println(i++ + ". " + acct.toString());
 		System.out.print("Select a source Cash Account by their number (integer): ");		
-		int in = getSc().nextInt();
+		in = getSc().nextInt();
 		
 		while ( !isValid(1, i, in) ){
 			System.out.println("Invalid input, please try again");
