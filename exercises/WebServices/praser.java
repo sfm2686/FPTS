b6a4b192c6d526a732306d511a90a0e890cd4392
Sultan Mira <sfm2686@g.rit.edu>
2016-07-05 17:50:25 -0400

    stocks is now parsed, organize it again

---
+++ exercises/WebServices/praser.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package WebServices;
+
+/**
+ * @authors Sultan Mira, Hunter Caskey
+ *
+ */
+public class praser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
