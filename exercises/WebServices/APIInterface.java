a32492488cda16a11e8689ac5559af4cd4cf3033
huntercaskey <hunterrcaskey@gmail.com>
2016-07-05 19:54:58 -0400

    Finished pair programming the WebServices exercise.

--- exercises/WebServices/APIInterface.java
+++ exercises/WebServices/APIInterface.java
@@ -1,40 +1,91 @@
 package WebServices;
 
 import java.io.BufferedReader;
-import java.io.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
 
 public class APIInterface {
 	
-	public static void main(String[] args) throws IOException {
-		
-		String url = "http://query.yahooapis.com/v1/public/yql?q=select%20LastTradePriceOnly%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)&env=store://datatables.org/alltableswithkeys";
+	/**
+	 * Class Attributes
+	 * Partial strings for the URL to Yahoo Financial Services in order to be able to insert a given ticker symbol.
+	 */
+	private static final String url1 = "http://query.yahooapis.com/v1/public/yql?q=select%20LastTradePriceOnly%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22";
+	private static final String url2 = "%22)&env=store://datatables.org/alltableswithkeys";
 
-		// Create a URL and open a connection
-		URL YahooURL = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) YahooURL.openConnection();
-
-		// Set the HTTP Request type method to GET (Default: GET)
-		con.setRequestMethod("GET");
-		con.setConnectTimeout(10000);
-		con.setReadTimeout(10000);
-
-		// Created a BufferedReader to read the contents of the request.
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuilder response = new StringBuilder();
-		while ((inputLine = in.readLine()) != null) { 
-			response.append(inputLine + "\n");
+	/**
+	 * getPrice is used by the DOW class in order to retrieve a price for a given component stock. 
+	 * @param tickerSymbol The identifier of a stock that is a component of the DOW.
+	 * @return The ticker symbol's corresponding price.
+	 * @throws IOException 
+	 */
+	protected static double getPrice(String tickerSymbol) {
+		String url = url1 + tickerSymbol + url2;
+		String xmlData = getXML(url);
+		return parseXML(xmlData);
+	}
+	
+	/**
+	 * parseXML will return the price of a ticker symbol parsed from the XML file that the Yahoo Financial Service API returns.
+	 * @param xml The given XML file in string form.
+	 * @return The parsed price from a given XML file.
+	 */
+	private static double parseXML(String xml){
+		if(!xml.contains("<LastTradePriceOnly>") || !xml.contains("</LastTradePriceOnly>")){
+			return 0.0;
 		}
-		// MAKE SURE TO CLOSE YOUR CONNECTION
-		in.close();
-
+		xml = xml.replace("</LastTradePriceOnly>", "<LastTradePriceOnly>");
+		return Double.parseDouble(xml.split("<LastTradePriceOnly>")[1]);
+	}
+	
+	/**
+	 * getXML connects to Yahoo Financial Services API to request a ticker symbol's associated price in the form of an XML file.
+	 * @param url The URL in string form that queries Yahoo for a stock's price.
+	 * @return An XML file in string form containing a stock's price.
+	 * @throws IOException
+	 */
+	private static String getXML(String url){
+		BufferedReader in = null;
+		StringBuilder response = null;;
+		try{
+			URL YahooURL = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) YahooURL.openConnection();
+	
+			// Set the HTTP Request type method to GET (Default: GET)
+			con.setRequestMethod("GET");
+			con.setConnectTimeout(10000);
+			con.setReadTimeout(10000);
+	
+			// Created a BufferedReader to read the contents of the request.
+			in = new BufferedReader(
+					new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			response = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) { 
+				response.append(inputLine + "\n");
+			}
+			in.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 		// response is the contents of the XML
-		System.out.println(response.toString());
+		return(response.toString());
+	}
+	
+	
+	protected static double getDOWDivisor(){
+		return 0.0;
+	}
+	
+	/**
+	 * Tests in Main Method
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		System.out.println(APIInterface.parseXML(APIInterface.getXML(url1 + "AAPL" + url2)));
 	}
 }
--- exercises/WebServices/DOW.java
+++ exercises/WebServices/DOW.java
@@ -3,8 +3,8 @@
  */
 package WebServices;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.io.*;
 
 /**
  * @authors Sultan Mira, Hunter Caskey
@@ -13,47 +13,113 @@ import java.util.Map;
 public class DOW {
 
 	private double currentPrice = 0.0;
-	private double dowDivisor = 0.14602128057775;
-	private Map<String, Double> dict; 
-	private String configPath = "./stocks.txt";
+	private double dowDivisor = 0.14602128057775; // Could not find an API to retrieve this dynamically
+	private String[] stocks; 
+	private String configPath = "stocks.txt";
 
 	public DOW(){
-		this.dict = new HashMap<String, Double>();
-		this.populateDict();
+		this.stocks = new String[30];
+		this.initStocks();
 	}
-	
-	private void populateDict(){
-		
-	}
+
 	
 	/**
-	 * Getter method to retrieve the current price of the DJIA
+	 * initStocks reads a configuration files (stocks.txt) and populates the stocks
+	 * array with the ticker symbols of the stocks that make of the DJIA.
+	 */
+	private void initStocks(){
+		File file = new File(this.configPath);
+		BufferedReader reader = null;
+		try{
+			reader =  new BufferedReader(new FileReader(file));
+			String text = "";
+			int i = 0;
+			while((text = reader.readLine()) != null){
+				this.stocks[i++] = text; 
+			}
+		}
+		catch(Exception e){
+			System.out.println("Parsing Error. System exiting.");
+			System.exit(1);
+		}
+		finally{
+			try{
+				if (reader != null){
+					reader.close();
+				}
+			}
+			catch(IOException e){}
+		}
+	}
+
+	/**
+	 * Getter method to retrieve the current price of the DJIA, 
+	 * to ensure the price is current it is reset prior to return.
 	 * @return The current price of the DJIA.
 	 */
 	public double getPrice(){
-		return 0.0;
+		this.setPrice();
+		return this.currentPrice;
 	}
-	
+
 	/**
 	 * Formula for calculating the DJIA is: DWIA = ∑(component stocks) / (DJIA divisor)
 	 */
 	private void setPrice(){
 		double sum = this.getIndexSum();
-		this.currentPrice = (sum / this.dowDivisor);
-		return;
-		
+		double price = (sum / this.dowDivisor);
+		this.currentPrice = (Math.round(price * 100.0) / 100.0);
 	}
-	
+
 	/**
 	 * Helper function to return the sum of prices for each component stock in the DJIA.
 	 * @return sum The sum of all prices for each component stock in the DJIA
 	 */
 	private double getIndexSum(){
-		double sum = 0;
-		for(double price : dict.values()){
-			sum += price;
+		double sum = 0.0;
+		for(String ticker : this.stocks){
+			sum += APIInterface.getPrice(ticker);
 		}
 		return sum;
 	}
 	
+	/**
+	 * Helper method for testing purposes.
+	 * @return
+	 */
+	private String listStockPrices(){
+		String str = "\n";
+		for(String stock : this.stocks){
+			str += "\tTicker: " + stock + ", Price: " + APIInterface.getPrice(stock) + "\n";
+		}
+		return str;
+	}
+
+	public static void main(String args[]) throws InterruptedException{
+		DOW dow = new DOW();
+		
+		Scanner input = new Scanner(System.in);
+		int time = -1;
+		do{
+			System.out.print("How often would you like to update the price of the DJIA? (seconds): ");
+			String in = input.next();
+			try{
+				time = Integer.parseInt(in);
+				time = Math.abs(time);
+			}
+			catch(Exception e){
+				System.out.println("Numeric integer values only.");
+			}
+		}while(time == -1);
+		System.out.println("Displaying the value of the DOW and it's component stocks every " + time + " second(s).");
+		input.close();
+		
+		while(true){
+			System.out.println("Price of the DOW's component stocks:");
+			System.out.println(dow.listStockPrices());
+			System.out.println("Price of DOW: " + dow.getPrice());
+			System.out.println("\n======================================\n");
+			Thread.sleep(1000 * time);
+		}
+	}
 }
