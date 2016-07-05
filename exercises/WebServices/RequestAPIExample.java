4255539821367bfd9b4c61dd7d0ffa1c3c41c1bc
huntercaskey <hunterrcaskey@gmail.com>
2016-07-05 17:54:56 -0400

    Merge

--- exercises/WebServices/RequestAPIExample.java
+++ exercises/WebServices/RequestAPIExample.java
@@ -10,11 +10,45 @@ import java.util.*;
 public class RequestAPIExample {
 	
 	private Map<String, Double> dict = new HashMap<String, Double>();
-	
+	private double dowDivisor = 0.14602128057775;
+	private double currentPrice = 0;
 	
 	private static void parser(){
 		;
 	}
+	
+	
+	/**
+	 * Getter method to retrieve the current price of the DJIA
+	 * @return The current price of the DJIA.
+	 */
+	public double getPrice(){
+		return 0.0;
+	}
+	
+	/**
+	 * Formula for calculating the DJIA is: DWIA = ∑(component stocks) / (DJIA divisor)
+	 */
+	private void setPrice(){
+		double sum = this.getIndexSum();
+		this.currentPrice = (sum / this.dowDivisor);
+		return;
+		
+	}
+	
+	/**
+	 * Helper function to return the sum of prices for each component stock in the DJIA.
+	 * @return sum The sum of all prices for each component stock in the DJIA
+	 */
+	private double getIndexSum(){
+		double sum = 0;
+		for(double price : dict.values()){
+			sum += price;
+		}
+		return sum;
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		String url = "http://query.yahooapis.com/v1/public/yql?q=select%20LastTradePriceOnly%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)&env=store://datatables.org/alltableswithkeys";
 
