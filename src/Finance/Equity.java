cdaca625ded132e2e13a61e3842a0dd6ebd4f83d
huntercaskey <hunterrcaskey@gmail.com>
2016-07-12 20:22:51 -0400

    Working on refactoring of Equity

--- src/Finance/Equity.java
+++ src/Finance/Equity.java
@@ -2,7 +2,6 @@ package Finance;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import CSV.EquityUtil;
 
 /**
  * @authors Sultan Mira, Hunter Caskey
@@ -14,8 +13,26 @@ import CSV.EquityUtil;
  */
 public abstract class Equity implements Holding, Serializable {
 
+	private String name;
 	private int numShares;
+	private ArrayList<Equity> children;
+	
+	// Operations for manipulating the composite relationships
+	
+	public void add(Equity child){
+		boolean present = false;
+		for(Equity element : this.children){
+			if(element.equals(child)){
+				present = true;
+			}
+		}
+		if(!present){
+			this.children.add(child);
+		}
+	}
+	
 
+	// Operations for accessing/manipulating the number of shares
 	protected void setNumShares(int shares) {
 		this.numShares = shares;
 	}
@@ -35,10 +52,12 @@ public abstract class Equity implements Holding, Serializable {
 	public void addShares(int numShares) {
 		this.numShares += numShares;
 	}
+	
+	public boolean equals(Equity comparison){
+		return(this.name.equals(comparison.getName()));
+	}
 
-	abstract double getPrice();
-
-	public abstract EquityUtil getReference();
-
+	public abstract double getPrice();
 	public abstract String getName();
+	public abstract double getValue();
 }
