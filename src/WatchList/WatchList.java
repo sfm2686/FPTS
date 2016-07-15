af37614f9fa27e2033e5c45c4ac9706057c705cb
huntercaskey <hunterrcaskey@gmail.com>
2016-07-15 11:31:11 -0400

    Finished the sort in WLPriortyIterator, completely commented the inner class.

--- src/WatchList/WatchList.java
+++ src/WatchList/WatchList.java
@@ -79,27 +79,64 @@ public class WatchList extends Observable implements Serializable{
 	
 	
 	//----------------------------------ITERATOR-----------------------------------//
+	
+	/**
+	 * WLPriorityIterator is a private iterator for the WatchList class. 
+	 * 
+	 * The collection to be iterated over is traversed by priority of the element's 
+	 * states.
+	 * 
+	 * @authors Sultan Mira, Hunter Caskey
+	 */
 	private class WLPriorityIterator implements Iterator{
 
+		//**** Class Attributes ****//
 		private int currentIndex = 0;
 		private ArrayList<WatchListItem> sorted;
 		
+		/**
+		 * Constructor for the WatchList's Iterator.
+		 * 
+		 * Initializes this class's private 'copy' of the data being iterated over, 
+		 * and then uses the WatchList's items collection to populated the sorted variable.
+		 */
 		public WLPriorityIterator() {
 			sorted = new ArrayList<WatchListItem>();
+			this.sort((ArrayList<WatchListItem>) items.clone());
 		}
 		
+		/**
+		 * Stock method from GOF Iterator pattern. 
+		 * 
+		 * first returns the very first element in the structure that is being iterated over.
+		 * 
+		 * @return The element in in the collection corresponding to index 0.
+		 */
 		@Override
 		public WatchListItem first() {
 			return this.sorted.get(0);
 		}
 
 
+		/**
+		 * Stock method from GOF Iterator pattern. 
+		 * 
+		 * next increases the index tracker of the list we are iterating over by one.
+		 */
 		@Override
 		public void next() {
 			this.currentIndex ++;
 		}
 
 
+		/**
+		 * Stock method from GOF Iterator pattern.
+		 *  
+		 * currentItem simply returns the item corresponding to the current position
+		 * of the iteration.
+		 * 
+		 * @return A WatchListItem.
+		 */
 		@Override
 		public WatchListItem currentItem() {
 			if ( !isDone() )
@@ -107,23 +144,75 @@ public class WatchList extends Observable implements Serializable{
 			return null;
 		}
 
+		/**
+		 * Stock method from GOF Iterator pattern. 
+		 * 
+		 * isDone checks whether the iterator object has fully gone through the 
+		 * object structure being navigated.
+		 * 
+		 * @return A boolean value, true if the iteration is complete.
+		 */
 		@Override
 		public boolean isDone() {
 			return ( this.currentIndex == this.sorted.size() );
 		}
 		
+		/**
+		 * Helper function for sorting a copy of WatchList's collection of
+		 * WatchListItems. This function sorts based upon priority, using the nextState 
+		 * helper method.
+		 * 
+		 * @param copy A shallow copy of the WatchList's collections. 
+		 */
+		private void sort(ArrayList<WatchListItem> copy){
+			WatchListItem.State state = WatchListItem.State.NowHigh;			
+			int tempIndex;
+			while(copy.size() > 0){
+				tempIndex = -1;
+				for(WatchListItem item : copy){
+					if(item.getState() == state){
+						tempIndex = copy.indexOf(item);
+						break;
+					}
+				}
+				if(tempIndex == -1){
+					state = nextState(state);
+				}
+				else{
+					sorted.add(copy.get(tempIndex));
+					copy.remove(tempIndex);
+				}
+			}
+		}
+		
+		/**
+		 * nextState aids in the sorting algorithm by deciding what the next appropriate
+		 * state to sort is. The WatchListItems are sorted by their state, and that order is:
+		 * NowHigh, NowLow, WasHigh, WasLow, and Normal.
+		 * 
+		 * @param state The current type of WatchListItem's state that is being sorted.
+		 * @return The state of WatchListItems to sort next.
+		 */
+		private WatchListItem.State nextState(WatchListItem.State state){
+			if(state == WatchListItem.State.NowHigh){
+				return(WatchListItem.State.NowLow);
+			}
+			else if(state == WatchListItem.State.NowLow){
+				return(WatchListItem.State.WasHigh);
+			}
+			else if(state == WatchListItem.State.WasHigh){
+				return(WatchListItem.State.WasLow);
+			}			
+			else if(state == WatchListItem.State.WasLow){
+				return(WatchListItem.State.Normal);
+			}
+			else{
+				return null;
+			}
+		}
+		
 	}
+	
 	//----------------------------------ITERATOR-----------------------------------//
-
 	
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-
 }
